d7c4fb2eb936231ea9402625cfdd5eb7cb4ab0bb
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2022-11-27 16:17:23 -0300

    Adding dividir tests

--- test/tp/core_test.clj
+++ test/tp/core_test.clj
@@ -5,5 +5,9 @@
 (deftest dividir-test
   (testing "Prueba de la funcion: dividir"
     (is (= 4 (dividir 12 3)))
+    (is (= 4.0 (dividir 12.0 3)))
     (is (= 4.0 (dividir 12.0 3.0)))
-    ))
+    (is (= 0 (dividir 1 2)))
+    (is (= 0.5 (dividir 1 2.0)))
+    )
+  ))
