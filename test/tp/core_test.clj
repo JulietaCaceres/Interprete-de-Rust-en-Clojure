1405bec887739c9bc495265d2eeed4386a550d66
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2022-11-30 23:29:07 -0300

    Agrego implementacio y tests de funciones faltantes. Mas caso de prueba de archivos rusts

--- test/tp/core_test.clj
+++ test/tp/core_test.clj
@@ -1,6 +1,39 @@
-(ns tp.core-test
+(ns tp.core_test
   (:require [clojure.test :refer :all]
             [tp.core :refer :all]))
+
+
+(deftest add_enter-test
+  (testing "Prueba de la funcion:  add_enter"
+      (is (= (with-out-str (listar (list 'fn 'main (symbol "(") (symbol ")") (symbol "{") 'println! (symbol "(") "Hola, mundo!" (symbol ")") (symbol "}"))))  "(fn main ( ) \n{\n println! ( Hola, mundo! ) \n}\n)\r\n"))
+      )
+    )
+
+
+(deftest agregar-ptocoma-test
+  (testing "Prueba de la funcion:  agregar-ptocoma"
+    (is (= (with-out-str(agregar-ptocoma (list  (symbol "}") 'renglon ) ))(with-out-str(agregar-ptocoma (list  (symbol "}")  (symbol ";") 'renglon ) ))))
+    )
+  )
+
+
+(deftest palabra-reservada?-test
+  (testing "Prueba de la funcion: palabra-reservada?"
+    (is (= true  (palabra-reservada? 'while)))
+    (is (= false (palabra-reservada? 'until)))
+    (is (= false (palabra-reservada? 13)))
+    )
+  )
+
+(deftest identificador?-test
+  (testing "Prueba de la funcion: identificador?"
+    (is (= true  (identificador? 'boolean)))
+    (is (= false (identificador? 'bool)))
+    (is (= true (identificador? 'e120)))
+    (is (= false (identificador? '12e0)))
+    )
+  )
+
 
 (deftest dividir-test
   (testing "Prueba de la funcion: dividir"
@@ -11,6 +44,7 @@
     (is (= 0.5 (dividir 1 2.0)))
     )
   )
+
 
 (deftest compatibles?-test
   (testing "Prueba de la funcion: compatibles?"
@@ -23,7 +57,7 @@
     (is (= false (compatibles? 'bool "Hola")))
     (is (= true (compatibles? 'bool true)))
     (is (= true (compatibles? 'usize 1)))
-    (is (= true (compatibles? 'char 'a)))
+    (is (= false (compatibles? 'char 'a)))
     (is (= true (compatibles? 'char ['a])))
     )
   )
@@ -50,22 +84,6 @@
     )
   )
 
-(deftest palabra-reservada?-test
-  (testing "Prueba de la funcion: palabra-reservada?"
-    (is (= true  (palabra-reservada? 'while)))
-    (is (= false (palabra-reservada? 'until)))
-    (is (= false (palabra-reservada? 13)))
-    )
-  )
-
-(deftest identificador?-test
-  (testing "Prueba de la funcion: identificador?"
-    (is (= true  (identificador? 'boolean)))
-    (is (= false (identificador? 'bool)))
-    (is (= true (identificador? 'e120)))
-    (is (= false (identificador? '12e0)))
-    )
-  )
 
 
 (deftest ya-declarado-localmente?-test
@@ -77,21 +95,33 @@
     )
   )
 
+(deftest buscar-tipo-de-retorno-test
+  (testing "Prueba de la funcion: buscar-tipo-de-retorno"
+    (is (= 'i64 (buscar-tipo-de-retorno [(symbol ";") (list 'println! (symbol "(") "La suma de 5 mas 7 es {}" (symbol ",") 'suma (symbol "(") 5 (symbol ",") 7 (symbol ")") (symbol ")") (symbol ";") (symbol "}")) ['fn 'suma (symbol "(") 'x (symbol ":") 'i64 (symbol ",") 'y (symbol ":") 'i64 (symbol ")") (symbol "->") 'i64 (symbol "{") 'x '+ 'y (symbol "}") 'fn 'main (symbol "(") (symbol ")") (symbol "{") 'suma (symbol "(") 5 (symbol ",") 7 (symbol ")")] :sin-errores [[0 2] [['suma ['fn [(list ['x (symbol ":") 'i64] ['y (symbol ":") 'i64]) 'i64]] 2] ['main ['fn [() ()]] 8]]] 0 [['CAL 8] 'HLT ['POPARG 1] ['POPARG 0] ['PUSHFM 0] ['PUSHFM 1] 'ADD 'RET ['PUSHFI 5] ['PUSHFI 7] ['CAL 2]] [[2 ['i64 nil] ['i64 nil]] [8]]] 2)) )
+    (is (= nil (buscar-tipo-de-retorno [(symbol ";") (list 'println! (symbol "(") "La suma de 5 mas 7 es {}" (symbol ",") 'suma (symbol "(") 5 (symbol ",") 7 (symbol ")") (symbol ")") (symbol ";") (symbol "}")) ['fn 'suma (symbol "(") 'x (symbol ":") 'i64 (symbol ",") 'y (symbol ":") 'i64 (symbol ")") (symbol "->") 'i64 (symbol "{") 'x '+ 'y (symbol "}") 'fn 'main (symbol "(") (symbol ")") (symbol "{") 'suma (symbol "(") 5 (symbol ",") 7 (symbol ")")] :sin-errores [[0 2] [['suma ['fn [(list ['x (symbol ":") 'i64] ['y (symbol ":") 'i64]) 'i64]] 2] ['main ['fn [() ()]] 8]]] 0 [['CAL 8] 'HLT ['POPARG 1] ['POPARG 0] ['PUSHFM 0] ['PUSHFM 1] 'ADD 'RET ['PUSHFI 5] ['PUSHFI 7] ['CAL 2]] [[2 ['i64 nil] ['i64 nil]] [8]]] 1)))
+    )
+  )
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; CARGAR-EN-ULT-REG: Recibe un vector de registros de activacion, una direccion, un tipo y un valor. Devuelve el
-; vector de registros de activacion con el ultimo registro actualizado, en la direccion indicada, con el nuevo tipo
-; y el nuevo valor.
-; Por ejemplo:
-; user=> (cargar-en-ult-reg [[['String "2"] ['i64 6] ['i64 2] ['i64 3] ['i64 0]] [['i64 nil] ['i64 nil]]] 1 'i64 0)
-; [[[String "2"] [i64 6] [i64 2] [i64 3] [i64 0]] [[i64 nil] [i64 0]]]
-;                                                             ^^^ ^
-; user=> (cargar-en-ult-reg [[['String "2"] ['i64 6] ['i64 2] ['i64 3] ['i64 0]] [['i64 nil] ['i64 0]]] 0 'f64 3)
-; [[[String "2"] [i64 6] [i64 2] [i64 3] [i64 0]] [[f64 3] [i64 0]]]
-;                                                   ^^^ ^
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(deftest ya-declarado-localmente?-test
-  (testing "Prueba de la funcion: ya-declarado-localmente?"
-    (is (= [[[String "2"] [i64 6] [i64 2] [i64 3] [i64 0]] [[i64 nil] [i64 0]]]   (cargar-en-ult-reg [[['String "2"] ['i64 6] ['i64 2] ['i64 3] ['i64 0]] [['i64 nil] ['i64 nil]]] 1 'i64 0)
-           ))
+(deftest convertir-formato-impresion-test
+  (testing "Prueba de la funcion: convertir-formato-impresion"
+    (is (=  (convertir-formato-impresion '("Hola, mundo!")) (list "Hola, mundo!") ))
+    (is (=  (convertir-formato-impresion '("- My name is {}, James {}.\n- Hello, {}{}{}!" "Bond" "Bond" 0 0 7)) (list "- My name is %s, James %s.\n- Hello, %d%d%d!" "Bond" "Bond" 0 0 7) ))
+    (is (=  (convertir-formato-impresion '("Las raices cuadradas de {} son +{:.8} y -{:.8}" 4.0 1.999999999985448 1.999999999985448)) (list "Las raices cuadradas de %.0f son +%.8f y -%.8f" 4.0 1.999999999985448 1.999999999985448) ))
   ))
+
+
+
+(deftest cargar-en-ult-reg-test
+  (testing "Prueba de la funcion: convertir-formato-impresion"
+    (is (=  (cargar-en-ult-reg [[['String "2"] ['i64 6] ['i64 2] ['i64 3] ['i64 0]] [['i64 nil] ['i64 nil]]] 1 'i64 0) [[['String "2"] ['i64 6] ['i64 2] ['i64 3] ['i64 0]] [['i64 nil] ['i64 0]]] ))
+    (is (=  (cargar-en-ult-reg [[['String "2"] ['i64 6] ['i64 2] ['i64 3] ['i64 0]] [['i64 nil] ['i64 0]]] 0 'f64 3) [[['String "2"] ['i64 6] ['i64 2] ['i64 3] ['i64 0]] [['f64 3] ['i64 0]]] ))
+
+    ))
+
+
+(deftest cargar-en-reg-dest-test
+  (testing "Prueba de la funcion: cargar-en-reg-dest"
+    (is (=  (cargar-en-reg-dest [ [['String "2"] ['i64 6] ['i64 2] ['i64 2] ['i64 2]][['i64 6] ['i64 2] ['i64 [0 3]] ['i64 [0 4]] ['i64 2] ['i64 2]]] [0 4] 'i64 0) [[['String "2"] ['i64 6] ['i64 2] ['i64 2] ['i64 0]] [['i64 6] ['i64 2] ['i64 [0 3]] ['i64 [0 4]] ['i64 2] ['i64 2]]] ))
+    (is (=  (cargar-en-reg-dest [[['String "2"] ['i64 6] ['i64 2] ['i64 2] ['i64 0]] [['i64 6] ['i64 2] ['i64 [0 3]] ['i64 [0 4]] ['i64 2] ['i64 2]]] [0 3] 'f64 3) [[['String "2"] ['i64 6] ['i64 2] ['f64 3] ['i64 0]] [['i64 6] ['i64 2] ['i64 [0 3]] ['i64 [0 4]] ['i64 2] ['i64 2]]] ))
+
+    ))
